96b9ec6ccb81cc89179e629bbd52049fa14dd7e8
Eben Stine <ebenstine@gmail.com>
2021-07-11 14:20:45 -0500

    fixed a few formatting things

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -54,5 +54,6 @@ console.log('Updated shoe count is', me.shoeCount);
   - Cool huh? It works, even though it wasn't there before
   - Console.log your object
 */
+
 me.favoriteColor = 'blue'
 console.log(me);
